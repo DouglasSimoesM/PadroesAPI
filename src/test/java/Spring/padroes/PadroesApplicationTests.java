c94d79bd46040b12d93198047320f34cf2978ae6
DouglasSimoesM <douglas.simoesm@gmail.com>
2025-01-02 11:21:54 -0300

    Design Patterns

---
+++ src/test/java/Spring/padroes/PadroesApplicationTests.java
@@ -0,0 +1,13 @@
+package Spring.padroes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadroesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
